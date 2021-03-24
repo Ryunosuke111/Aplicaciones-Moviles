665904244ee75a37a455a4a802ad0ccd5b5a9739
Ryunosuke111 <jeyadrey16@gmail.com>
2021-03-24 12:32:26 -0500

    Signed-off-by: Ryunosuke111 <jeyadrey16@gmail.com>

--- app/src/main/java/com/example/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/myapplication/Main3Activity.java
@@ -1,17 +1,31 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main3Activity extends AppCompatActivity {
     TextView recibir;
+    Button volver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         recibir = findViewById(R.id.recibir);
+        volver = findViewById(R.id.bt6);
+
+        Bundle datosRecibidos = getIntent().getExtras();
+        String dato = datosRecibidos.getString("mensajes").toString();
+        recibir.setText(dato);
     }
-    public void 
+    public void  volver1(View v){
+        Intent ir = new Intent(this,Main2Activity.class);
+        ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(ir);
+    }
+
 }
